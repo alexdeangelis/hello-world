cd8ee6a8b379b06510d5bd53a3f0698538644124
Alex De Angelis <alex@activeimage.co.uk>
2018-03-15 15:32:44 +0000

    JavaScript for Beginners

---
+++ JavaScript/JavaScript Tutorial for Beginners/scripts/jsEvents.js
@@ -0,0 +1,32 @@
+//https://www.youtube.com/watch?v=e57ReoUn6kM
+
+var numOne = document.getElementById('num-one');
+
+var numTwo = document.getElementById('num-two');
+
+var addSum = document.getElementById('add-sum');
+
+//numOne.addEventListener("click",function() {
+//    alert('hi');
+//})
+
+//numOne.addEventListener("mouseenter",function() {
+//    alert('hi');
+//})
+
+//numOne.addEventListener("mouseleave",function() {
+//    alert('hi');
+//})
+
+//numOne.addEventListener("focus",function() {
+//    alert('hi');
+//})
+
+//numOne.addEventListener("blur",function() {
+//    alert('hi');
+//})
+
+//input event looks for anything changing in the value of an input
+numOne.addEventListener("input",function() {
+    alert('hi');
+})
