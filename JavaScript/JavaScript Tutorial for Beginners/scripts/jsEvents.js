92a5c538c924ded7a8ba57395a1349d6f32de943
Alex De Angelis <alex@activeimage.co.uk>
2018-03-16 07:40:40 +0000

    Add files via upload

--- JavaScript/JavaScript Tutorial for Beginners/scripts/jsEvents.js
+++ JavaScript/JavaScript Tutorial for Beginners/scripts/jsEvents.js
@@ -26,7 +26,48 @@ var addSum = document.getElementById('add-sum');
 //    alert('hi');
 //})
 
-//input event looks for anything changing in the value of an input
-numOne.addEventListener("input",function() {
-    alert('hi');
-})
+////input event looks for anything changing in the value of an input
+//numOne.addEventListener("input",add);
+//numTwo.addEventListener("input",add);
+//
+//function add() {
+//    var one = parseFloat(numOne.value) || 0;
+//    var two = parseFloat(numTwo.value) || 0;
+//    //console.log(one,two);
+//    //var sum = one + two;
+//    addSum.innerHTML = "your sum is: " + (one+two);
+//}
+
+var simon = document.getElementById('simon');
+var bruce = document.getElementById('bruce');
+var ben = document.getElementById('ben');
+
+simon.addEventListener('click', picLink);
+bruce.addEventListener('click', picLink);
+ben.addEventListener('click', picLink);
+
+function picLink() {
+    var allImages = document.querySelectorAll("img");
+    
+    for(var i = 0; i< allImages.length; i++){
+        allImages[i].className="hide";
+    }
+    
+    //console.log(this);
+    var picId = this.attributes["data-img"].value;
+    //console.log(picId);
+    var pic = document.getElementById(picId);
+    
+    if (pic.className === "hide") {
+        pic.className = "";
+    } else {
+        pic.className = "hide";
+    }
+    
+}
+
+
+
+
+
+
