fc03174cd32f0d3f0d4d81c5f339399437802317
Alex De Angelis <alex.deangelis@goodmanfox.com>
2018-03-23 11:30:50 +0000

    events2

--- JavaScript/JavaScript Tutorial for Beginners/scripts/jsEvents.js
+++ JavaScript/JavaScript Tutorial for Beginners/scripts/jsEvents.js
@@ -31,7 +31,10 @@ var addSum = document.getElementById('add-sum');
 //numTwo.addEventListener("input",add);
 //
 //function add() {
-//    var one = parseFloat(numOne.value) || 0;
+//    //Anything that comes from an input is a string.
+//    //parseFloat() turns the string into a number
+//    //The || is OR. If the first value doesn't create a number, use 0 instead
+//    var one = parseFloat(numOne.value) || 0; 
 //    var two = parseFloat(numTwo.value) || 0;
 //    //console.log(one,two);
 //    //var sum = one + two;
@@ -42,30 +45,42 @@ var simon = document.getElementById('simon');
 var bruce = document.getElementById('bruce');
 var ben = document.getElementById('ben');
 
-simon.addEventListener('click', picLink);
-bruce.addEventListener('click', picLink);
-ben.addEventListener('click', picLink);
+simon.addEventListener('click', picLink)
+bruce.addEventListener('click', picLink)
+ben.addEventListener('click', picLink)
+
 
 function picLink() {
-    var allImages = document.querySelectorAll("img");
     
-    for(var i = 0; i< allImages.length; i++){
-        allImages[i].className="hide";
-    }
+    //console.log(this); 
+    //this shows the element that has been clicked with this function
+    var picID = this.attributes['data-img'].value;
+    //this gets the data-img property from the element clicked
+    //.value gets the value of the specific property
+    //console.log(picID); 
+    var pic = document.getElementById(picID);
+    //this gets the element that has an id that matches the value of the data-img property
+    //console.log(pic);
     
-    //console.log(this);
-    var picId = this.attributes["data-img"].value;
-    //console.log(picId);
-    var pic = document.getElementById(picId);
-    
-    if (pic.className === "hide") {
-        pic.className = "";
+    if(pic.className == 'hide') {
+        hideAll();
+        pic.className = '';
     } else {
-        pic.className = "hide";
+        hideAll();
     }
     
 }
 
+function hideAll() {
+    //var allImages = document.querySelectorAll('img');
+    var allImages = document.getElementsByTagName('img');
+    //this gets all the images on the page
+    console.log(allImages);
+    //give all images the class of hide, before anything else happens
+    for(var i = 0; i < allImages.length; i++) {
+        allImages[i].className = 'hide';
+    }
+}
 
 
 
