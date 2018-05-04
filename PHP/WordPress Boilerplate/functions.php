4d0880f363a716fa987233b81e8e88db7e82bb8b
Alex De Angelis <alex@activeimage.co.uk>
2018-05-04 16:41:49 +0100

    boilerplate update

--- PHP/WordPress Boilerplate/functions.php
+++ PHP/WordPress Boilerplate/functions.php
@@ -19,3 +19,7 @@ if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page();
 	
 }
+
+//Enquing Bootstrap JS in the footer & loading jQuery as a dependency
+//For more info, go here: https://digwp.com/2009/06/including-jquery-in-wordpress-the-right-way/
+wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'), null, true);
--- PHP/WordPress Boilerplate/header.php
+++ PHP/WordPress Boilerplate/header.php
@@ -32,3 +32,43 @@ $themeURL = get_template_directory_uri();
     </head>
     
     <body <?php body_class(); ?>>
+        
+        <!--Here's a standard nav, modified from the Bootstrap example-->
+        
+        <div class="container">
+
+                <!-- Static navbar -->
+                <nav class="navbar navbar-default">
+
+
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar top-bar"></span>
+                                <span class="icon-bar middle-bar"></span>
+                                <span class="icon-bar bottom-bar"></span>
+                            </button>
+                            
+                            
+                            
+                            
+                            <div class="logo">
+                                <a href="/">
+                                                                        
+                                    <img src="https://dcnetworks.ie/wp/wp-content/uploads/2017/11/placeholder-logo-2.png"/>
+                                                                        
+                                </a>
+                            </div>
+                        </div>
+
+                        <div id="navbar" class="navbar-collapse collapse">
+
+                            <div class="menu-main-menu-container">
+                                <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+                            </div>
+                        </div><!--/.nav-collapse -->
+                </nav>
+
+
+
+            </div> <!-- /container -->
